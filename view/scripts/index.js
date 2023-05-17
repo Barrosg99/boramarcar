4ca8a1f88408a5191c8dcfabd50492b4d95398d8
Beatriz Sakai <biasakai@outlook.com>
2023-05-17 19:03:49 -0300

    feat: adiciona "olá pessoa" e criar evento no menu

--- view/scripts/index.js
+++ view/scripts/index.js
@@ -13,11 +13,18 @@ if(token) {
     button.innerText = "Minha conta";
     const ul = document.createElement("ul");
     ul.className = "dropdown-menu";
+    const li = document.createElement("li");
+    li.className = "dropdown-item";
+    li.innerText = `Olá, ${userInfo.Nome_Razao_Social}`;
+    ul.appendChild(li);
+
     const opcoes = [ 
+        { href: "#", text: "Criar evento" },
         { href: "#", text: "Meus eventos" },
         { href: "#", text: "Configurações" },
         { href: "#", text: "Sair" }
     ];
+
     for (const opcao of opcoes) {
         const li = document.createElement("li");
         const a = document.createElement("a");
